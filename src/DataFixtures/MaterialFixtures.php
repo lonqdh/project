16cfca3e6b4e2236a76efdceb63b1d1baca71e74
lonqdh <longndhgch210216@fpt.edu.vn>
2022-08-14 17:18:11 +0700

    update material fixture

--- src/DataFixtures/MaterialFixtures.php
+++ src/DataFixtures/MaterialFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Material;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,22 @@ class MaterialFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        $chatlieu = ["Cotton","Wool","Silk","Leather","Denim"];
+        $mau = ["White","Blue","Yellow","Orange","Black"];
+        $vai = ["Polyester","Nylon","Acrylic"];
+        $texture = ["Rough","Smooth","Soft","Light"];
+        for ($i=0; $i < 5; $i++) { 
+            $key = array_rand($chatlieu,1);
+            $keymau = array_rand($mau,1);
+            $keyvai = array_rand($vai,1);
+            $keytexture = array_rand($texture,1);
+            $material = new Material;
+            $material->setName($chatlieu[$key])
+                        ->setColor($mau[$keymau])
+                        ->setTexture($texture[$keytexture])
+                        ->setFabric($vai[$keyvai]);
+            $manager->persist($material);
+        }
         $manager->flush();
     }
 }
