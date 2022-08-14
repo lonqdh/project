5383157c5e034404410d68006e6872b58bff16ca
lonqdh <longndhgch210216@fpt.edu.vn>
2022-08-14 18:23:48 +0700

    code fixture designer category manufac

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,19 @@ class CategoryFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $category = ["T-Shirt","Dress","Short"];
+        $anh = ["https://media1.popsugar-assets.com/files/thumbor/Y6bXcbnD2g1jMbkcbAVaPt0u-ps/fit-in/728xorig/filters:format_auto-!!-:strip_icc-!!-/2016/03/30/634/n/1922564/3b4e46771b2b4218_682443_ou_xl/i/Shop-Salma-Gucci-Dress.jpg",
+        "https://static.nike.com/a/images/t_default/yjmjfgvca8w01dcyzokk/sportswear-t-shirt-zmMkxS.png",
+        "https://assets.adidas.com/images/w_600,f_auto,q_auto/67132742fd8849bc9574ac710179c38b_9366/Quan_short_Chelsea_3_Soc_AEROREADY_Essentials_Mau_xanh_da_troi_GL0023_21_model.jpg"];
+        for ($i=0; $i < 5; $i++) { 
+            $key = array_rand($category,1);
+            $keyanh = array_rand($anh,1);
+            $category = new Category;
+            $category->setName($category[$key])
+                        ->setImage($anh[$keyanh])
+                        ->setDescription("Types of clothes for you");
+            $manager->persist($category);
+        }
 
         $manager->flush();
     }
--- src/DataFixtures/ManufacturerFixtures.php
+++ src/DataFixtures/ManufacturerFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Manufacturer;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,20 @@ class ManufacturerFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $nhasanxuat = ["Good Clothing Company","Apparel Production Inc","Billoomi Fashion"];
+        $anh = ["https://fashinza.com/textile/wp-content/uploads/2021/10/uLn8UkHZ_400x400-370x370.jpeg",
+        "https://media-exp1.licdn.com/dms/image/C4D0BAQGDO1MaULF8tw/company-logo_200_200/0/1568076736534?e=2147483647&v=beta&t=WRDqHhBQaYt9XZ2Y0bcLchholcK9K4MRloo7McDZ4d0",
+        "http://media.releasewire.com/photos/show/?id=181471"];
 
+        for ($i=0; $i < 5; $i++) { 
+            $key = array_rand($nhasanxuat,1);
+            $keyanh = array_rand($anh,1);
+            $manufacturer = new Manufacturer;
+            $manufacturer->setName($nhasanxuat[$key])
+                        ->setImage($anh[$keyanh])
+                        ->setEmail("anhlonglichlam@gmail.com");
+            $manager->persist($manufacturer);
+        }
         $manager->flush();
     }
 }
