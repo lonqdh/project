11ec6e545083a1302009ee7333278dc11c5e8f4a
tonypassweb <tuyenddgch210710@fpt.edu.vn>
2022-08-15 14:05:08 +0700

    1

--- src/Controller/ManufacturerController.php
+++ src/Controller/ManufacturerController.php
@@ -2,17 +2,48 @@
 
 namespace App\Controller;
 
+use App\Entity\Manufacturer;
+use App\Repository\ManufacturerRepository;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/manufacturer')]
 class ManufacturerController extends AbstractController
 {
-    #[Route('/manufacturer', name: 'app_manufacturer')]
-    public function index(): Response
-    {
-        return $this->render('manufacturer/index.html.twig', [
-            'controller_name' => 'ManufacturerController',
-        ]);
+    #[Route('/index', name: 'manufacturer_index')]
+    public function manufacturerIndex () {
+        $manufacturers = $this->getDoctrine()->getRepository(Manufacturer::class)->findAll();
+        return $this->render('manufacturer/index.html.twig'
+    [
+       // 'manufacturers' => $manufacturers
+    ]);
+    }
+
+    #[Route('/detail/{id}', name: 'manufacturer_detail')]
+    public function manufacturerDetail ($id, ManufacturerRepository $manufacturerRepository) {
+        $manufacturer = $manufacturerRepository->find($id);
+        if($manufacturer ==null){
+            $this->addFlash('Warning','Invalid manufacturer');
+            return $this->redirectToRoute('manufacturer_index');
+        }
+        return $this->render('manufacturer/detail.html.twig'
+    [
+        // 'manufacturer' => $manufacturer
+    ]);
+    }
+
+    #[Route('/delete/{id}', name: 'manufacturer_delete')]
+    public function manufacturerDelete ($id, ManagerRegistry $managerRegistry){
+        $manufacturer = $managerRegistry->getRepository(Manufacturer::class)->find($id);
+        if ($manufacturer == null){
+            $this->addFlash('Warning','Manufacturer not exist!');
+        } else {
+            $manager = $managerRegistry->getManager();
+            $manager->remove($manufacturer);
+            $manager->flush();
+            $this->addFlash('Info','Delete manufacturer success!');
+        }
     }
 }
--- src/DataFixtures/BrandFixtures.php
+++ src/DataFixtures/BrandFixtures.php
@@ -10,16 +10,16 @@ class BrandFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $brand = ["Gucci","Louis Vuitton","Nike"];
+        $brandname = ["Gucci","Louis Vuitton","Nike"];
         $anh = ["https://menback.com/wp-content/uploads/2020/03/logo-gucci.jpg",
         "https://gigamall.com.vn/data/2019/09/05/15023424_LOGO-NIKE-500x500.jpg",
         "https://caodangytehadong.edu.vn/wp-content/uploads/thuong-hieu-thoi-trang-noi-tieng-1.jpg"];
 
         for ($i=0; $i < 3; $i++) { 
-            $key = array_rand($brand,1);
+            $key = array_rand($brandname,1);
             $keyanh = array_rand($anh,1);
             $brand = new Brand;
-            $brand->setName($brand[$key])
+            $brand->setName($brandname[$key])
                         ->setImage($anh[$keyanh])
                         ->setTelephone("099999999")
                         ->setEmail("longhoang@gmail.com");
