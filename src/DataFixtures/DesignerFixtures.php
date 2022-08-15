64363976f1c0c632ed00aa1f5435200077859fdd
lonqdh <longndhgch210216@fpt.edu.vn>
2022-08-15 17:37:46 +0700

    Update DesignerFixtures.php

--- src/DataFixtures/DesignerFixtures.php
+++ src/DataFixtures/DesignerFixtures.php
@@ -11,9 +11,9 @@ class DesignerFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $designername = ["Bo Long dep trai","Thang Viet","Quy ngai Tony"];
-        $anh = ["https://taimeme.com/public/upload/memes/thay-giao-huan-hoa-hong-giang-bai-mon-giao-duc-xa-hoi-90d35fe69feef6385c1dfda341ea9b33.jpg",
-        "https://media.discordapp.net/attachments/875652964520448006/1004680120117772328/IMG_0434.jpg?width=527&height=702",
-        "https://camo.voz.tech/8bca714b8015086d538dddd86ce3f6cfeb7f696d/68747470733a2f2f692e696d6775722e636f6d2f594559545944702e706e67/"];
+        $anh = ["https://fn.vinhphuc.edu.vn/UploadImages/mnlienchau/admin/tho-dong-vat-3%20c%C3%BAn%20con.jpg",
+        "",
+        ""];
 
         for ($i=0; $i < 3; $i++) { 
             $key = array_rand($designername,1);
