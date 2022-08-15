05602cfa3df27c69f8a28dc0c1a75601bbfd4645
tonypassweb <tuyenddgch210710@fpt.edu.vn>
2022-08-15 14:42:31 +0700

    manufacturerController

--- src/Controller/ManufacturerController.php
+++ src/Controller/ManufacturerController.php
@@ -3,11 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Manufacturer;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\BrowserKit\Request;
 use App\Repository\ManufacturerRepository;
-use Symfony\Bridge\Doctrine\ManagerRegistry;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/manufacturer')]
 class ManufacturerController extends AbstractController
@@ -16,6 +17,15 @@ class ManufacturerController extends AbstractController
     public function manufacturerIndex () {
         $manufacturers = $this->getDoctrine()->getRepository(Manufacturer::class)->findAll();
         return $this->render('manufacturer/index.html.twig'
+    [
+       // 'manufacturers' => $manufacturers
+    ]);
+    }
+
+    #[Route('/list', name: 'manufacturer_list')]
+    public function manufacturerList () {
+        $manufacturers = $this->getDoctrine()->getRepository(Manufacturer::class)->findAll();
+        return $this->render('manufacturer/list.html.twig'
     [
        // 'manufacturers' => $manufacturers
     ]);
@@ -45,5 +55,48 @@ class ManufacturerController extends AbstractController
             $manager->flush();
             $this->addFlash('Info','Delete manufacturer success!');
         }
+        return $this->redirectToRoute('manufacturer_index');
     }
+
+    #[Route('/add', name:'manufacturer_add')]
+    public function manufacturerAdd (Request $request){
+        $manufacturer = new Manufacturer;
+        $form = $this->createForm(ManufacturerType::class, $manufacturer);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($manufacturer);
+            $manager->flush();
+            $this->addFlash('Info','Add manufacturer success');
+            return $this->redirectToRoute('manufacturer_index');
+        }
+        return $this->renderForm('manufacturer/add.html.twig'
+    [
+        //'authorForm' => $form
+    ]);
+    }
+
+    #[Route('/edit/{$id}', name:'manufacturer_edit')]
+    public function manufacturerEdit ($id, Request $request){
+        $manufacturer = $this->getDoctrine()->getRepository(Manufacturer::class)->find($id);
+        if ($manufacturer == null){
+            $this->addFlash('Warning','Manufacturer not exist');
+            return $this->redirectToRoute('manufacturer_index');
+        } else {
+            $form = $this->createForm(ManufacturerType::class, $manufacturer);
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($manufacturer);
+                $manager->flush();
+                $this->addFlash('Info','Edit author successfully!');
+                return $this->redirectToRoute('manufacturer_index');
+            }
+            return $this->renderForm('manufacturer/edit.html.twig'
+        [
+            //'authorForm' => $form
+        ]);
+        }
+    }
+
 }
