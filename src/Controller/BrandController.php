265e70903c25e8dfc62abd9777af3daaa5f2f5c3
tonypassweb <tuyenddgch210710@fpt.edu.vn>
2022-08-16 16:45:34 +0700

    security

--- src/Controller/BrandController.php
+++ src/Controller/BrandController.php
@@ -9,11 +9,13 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/brand')]
 class BrandController extends AbstractController
 {
+    #[IsGranted("ROLE_ADMIN")]
     #[Route('/index', name: 'brand_index')]
     public function brandIndex()
     {
